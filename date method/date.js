b0e41f6a9197d1a9f4210469fe2758df823817bf
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 16:02:19 +0530

    date method.

---
+++ date method/date.js
@@ -0,0 +1,31 @@
+var x = Date();
+//var x = Date.now();
+//var x = new Date("December 25, 1995 23:15:30"); //returns a date object with the current date and time
+var x = new Date();2
+//console.log(x);
+//console.log(x.getDay());
+//console.log(x.getMonth());
+//console.log(x.getDate());
+//console.log(x.getDay()); // Sunday - Saturday : 0 - 6
+//console.log(x.getHours());
+//console.log(x.getMinutes());
+//console.log(x.getSeconds());
+//console.log(x.getMilliseconds());
+//console.log(x.getTime());
+//console.log(x.getUTCDate);
+
+const d = new Date();
+d.setDate(25);
+d.getDate();
+document.write(d);
+document.write("<br>");
+
+const d = new Date();
+d.getMonth();
+d.setMonth(0);
+document.write(d);
+document.write("<br>");*/
+
+
+
+
